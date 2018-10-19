564b6d7ae9de1bebb0fc03052cad578dcb48e636
ssatm <44239678+ssatm@users.noreply.github.com>
2018-10-19 23:55:45 +0300

    Create st3.js

---
+++ st3.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+const request = require('request');
+const fs = require('fs');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const prefix = ">"
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+  client.on('ready', async () => {
+      let ToRnEdO  = ["hi","spam","nigga"]
+  setInterval(() => {
+client.channels.get("489081550835548201").send(`${ToRnEdO [Math.floor(Math.random() *ToRnEdO .length)]}`)
+},1);
+});
+  
+
+
+
+client.on('message', message => {
+  if(message.author.bot) return;  
+
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+if (command == "say") {
+
+let rank = message.guild.member(message.author).roles.find('name', 'SAY');
+if (!rank) return message.reply('-_- go out')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+
+
+});
+  
+
+client.login(process.env.BOT_TOKEN);
